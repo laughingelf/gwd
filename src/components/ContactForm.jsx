aa42ccbbcbfac9ff970895adf7c8cf750550d80a
Justin Gery <jaygery90@gmail.com>
2025-04-15 12:55:06 -0500

    updated form component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,26 +2,47 @@ import { useState } from "react";
 
 
 const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: encode({'form-name': 'contact', name, email, phone, message}),
+        })
+        .then(() => alert("Success!"))
+        .catch((error) => alert(error))
+    };
+
+    const encode = (data) => {
+        return Object.keys(data)
+          .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+          .join('&');
+      };
 
     return (
-        <form method="POST" data-netlify="true" id="contact-form" name="contact">
+        <form onSubmit={handleSubmit} id="contact-form" name="contact">
             <input type="hidden" name="form-name" value="contact" />
                 <div class='form-group'>
                     <label for='name'>Name: </label>
-                    <input type='text' id='name' name='name' reqired/><br/><br/>
+                    <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} reqired/><br/><br/>
                 </div>
                 <div class='form-group'>
                     <label for='email'>Email:</label><br/>
-                    <input type='email' id='email' name='email' required/><br/><br/>
+                    <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required/><br/><br/>
                 </div>
                 <div class='form-group'>
                     <label for='phone'>Phone:</label><br/>
-                    <input type='tel' id='phone' name='phone' required/><br/><br/>
+                    <input type='tel' id='phone' name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} required/><br/><br/>
                 </div>
                 <div class='form-group'>
                     <label for='message'>Message:</label><br/>
-                    <textarea id='message' name='message' rows='5' required/><br/>
+                    <textarea id='message' name='message' rows='5' value={message}  onChange={(e) => setMessage(e.target.value)} required/><br/>
                 </div>
 
 
