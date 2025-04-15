aecd4e4a81d5d693e1347d98254fea2482fd5947
Justin Gery <jaygery90@gmail.com>
2025-04-15 13:23:52 -0500

    created custom success modal

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import SuccessModal from "./SuccessModal";
 
 
 const ContactForm = () => {
@@ -6,6 +7,8 @@ const ContactForm = () => {
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [message, setMessage] = useState('');
+
+    const [showModal, setShowModal] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -15,7 +18,7 @@ const ContactForm = () => {
             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
             body: encode({'form-name': 'contact', name, email, phone, message}),
         })
-        .then(() => alert("Success!"))
+        .then(() => setShowModal(true) )
         .then(() => setName(''), setEmail(''), setPhone(''), setMessage('') )
         .catch((error) => alert(error))
     };
@@ -27,29 +30,40 @@ const ContactForm = () => {
       };
 
     return (
-        <form onSubmit={handleSubmit} data-netlify-recapchta='true' id="contact-form" name="contact">
-            <input type="hidden" name="form-name" value="contact" />
-                <div class='form-group'>
-                    <label for='name'>Name: </label>
-                    <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} reqired/><br/><br/>
-                </div>
-                <div class='form-group'>
-                    <label for='email'>Email:</label><br/>
-                    <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required/><br/><br/>
-                </div>
-                <div class='form-group'>
-                    <label for='phone'>Phone:</label><br/>
-                    <input type='tel' id='phone' name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} required/><br/><br/>
-                </div>
-                <div class='form-group'>
-                    <label for='message'>Message:</label><br/>
-                    <textarea id='message' name='message' rows='5' value={message}  onChange={(e) => setMessage(e.target.value)} required/><br/>
-                </div>
+        <>
 
-                <div data-netlify-recaptcha='true'></div>
-                
-                <button class='form-button' type="submit" >Submit</button>
-        </form>
+            <SuccessModal
+                show={showModal}
+                onClose={() => setShowModal(false)}
+                title="GhostStack Designs"
+            >
+                <p>Thanks for reaching out to GhostStack Designs! We got your message and we’ll get back to you within 24 hours. Talk soon!</p>
+            </SuccessModal>
+        
+            <form onSubmit={handleSubmit} data-netlify-recapchta='true' id="contact-form" name="contact">
+                <input type="hidden" name="form-name" value="contact" />
+                    <div class='form-group'>
+                        <label for='name'>Name: </label>
+                        <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} reqired/><br/><br/>
+                    </div>
+                    <div class='form-group'>
+                        <label for='email'>Email:</label><br/>
+                        <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required/><br/><br/>
+                    </div>
+                    <div class='form-group'>
+                        <label for='phone'>Phone:</label><br/>
+                        <input type='tel' id='phone' name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} required/><br/><br/>
+                    </div>
+                    <div class='form-group'>
+                        <label for='message'>Message:</label><br/>
+                        <textarea id='message' name='message' rows='5' value={message}  onChange={(e) => setMessage(e.target.value)} required/><br/>
+                    </div>
+
+                    <div data-netlify-recaptcha='true'></div>
+                    
+                    <button class='form-button' type="submit" >Submit</button>
+            </form>
+        </>
     )
 }
 
---
+++ src/components/SuccessModal.jsx
@@ -0,0 +1,19 @@
+import "./Modal.css";
+
+const SuccessModal = ({ show, onClose, title, children }) => {
+  if (!show) return null;
+
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-box">
+        <h2 className="modal-title">{title}</h2>
+        <div className="modal-content">{children}</div>
+        <button onClick={onClose} className="modal-close-btn">
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessModal;
